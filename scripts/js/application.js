8f7970d79bf4dc47699f9125a174db2f016a222b
Roman Chaban <roman.chaban1001@gmail.com>
2023-12-13 00:27:25 +0200

    Add variables to application.js file

--- scripts/js/application.js
+++ scripts/js/application.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const headerMenuItems = document.querySelector(".nav__menu");
+const heroTitleItem = document.querySelector(".hero__title");
+const subscribeButton = document.querySelector(".subscribe");
+const companyMenuItems = document.querySelectorAll(
+  ".company__menu > .company__menu--item"
+);
+const legalMenuItems = document.querySelectorAll(
+  ".legal__menu > .legal__menu--item"
+);
+let homeProLogo = document.querySelectorAll(".homepro__logo");
+let viewButtonItem = document.querySelector('.view');
+
