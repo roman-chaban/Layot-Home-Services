f515565c1b92c2187d37e9d98d4952a2a9ab415e
Roman Chaban <roman.chaban1001@gmail.com>
2024-04-16 22:39:30 +0300

    Create Burger

--- scripts/js/application.js
+++ scripts/js/application.js
@@ -1,15 +1,22 @@
-"use strict";
+'use strict';
 
-const headerMenuItems = document.querySelector(".nav__menu");
-const heroTitleItem = document.querySelector(".hero__title");
-const subscribeButton = document.querySelector(".subscribe");
+const headerMenuItems = document.querySelector('.nav__menu');
+const heroTitleItem = document.querySelector('.hero__title');
+const subscribeButton = document.querySelector('.subscribe');
+let homeProLogo = document.querySelectorAll('.homepro__logo');
+let viewButtonItem = document.querySelector('.view');
+let professionalImage = document.querySelector('.professional__image');
+const headerBurgerButton = document.querySelector('.header__burger');
 const companyMenuItems = document.querySelectorAll(
-  ".company__menu > .company__menu--item"
+  '.company__menu > .company__menu--item'
 );
 const legalMenuItems = document.querySelectorAll(
-  ".legal__menu > .legal__menu--item"
+  '.legal__menu > .legal__menu--item'
 );
-let homeProLogo = document.querySelectorAll(".homepro__logo");
-let viewButtonItem = document.querySelector(".view");
-let professionalImage = document.querySelector(".professional__image");
 
+headerBurgerButton.style.background = 'transparent';
+const nav = document.querySelector('.nav');
+
+headerBurgerButton.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
